ebd4b6725232985917bb852aa98048c6dcace1f8
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2024-12-18 16:28:24 +0000

    version 0.0.27 exporting world facture editing/deleting

--- apps/facture/static/facture/js/localFactureTools.js
+++ apps/facture/static/facture/js/localFactureTools.js
@@ -39,7 +39,7 @@ function deleteItem(pk) {
       .then((response) => {
         if (response.ok) {
           alert("Facture deleted successfully!");
-          location.reload(); // Refresh the page or handle as needed
+          window.location.href = '/facture/local/';
         } else {
           alert("Failed to delete facture.");
         }
