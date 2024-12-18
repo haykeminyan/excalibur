c91555478f6228f223a1706009b3dc835f4108d6
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2024-12-18 23:09:53 +0000

    version 0.0.29 update strong password

--- excalibur/settings.py
+++ excalibur/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'excalibur.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'default_db_name'),
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'default_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'default_password'),
         'HOST': os.getenv('POSTGRES_HOST', 'database'),
