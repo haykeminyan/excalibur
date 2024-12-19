6bd6b7325ede44ca49e089788d4f57d5fabeca90
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2024-12-19 17:48:29 +0000

    version 0.0.31 try to enable https

--- excalibur/settings.py
+++ excalibur/settings.py
@@ -103,7 +103,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'options': '-c statement_timeout=10000'
         },
 
     },
