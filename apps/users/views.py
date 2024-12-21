9ff5c2f85114127f00f43137449fdc50cc6d091b
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2024-12-21 02:13:21 +0000

    version 0.0.32 added main page

--- apps/users/views.py
+++ apps/users/views.py
@@ -12,7 +12,7 @@ class LoginUser(LoginView):
     extra_context = {'title': 'Authorization'}
 
     def get_success_url(self):
-        return reverse_lazy('local_list')
+        return reverse_lazy('apps.facture:local_list')
 
 
 class LogoutUser(LogoutView):
