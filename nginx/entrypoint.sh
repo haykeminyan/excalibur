382769bdc8353f2fe0423646c02ffdd6bc0a6057
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2024-12-22 23:00:47 +0000

    version 0.0.32 trying to make https clearer

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start Nginx in the background
+nginx &
+
+# Wait for Nginx to start fully
+sleep 5
+
+# Request SSL certificates using Certbot and the Nginx plugin
+certbot --nginx -d pmsolution-facture.org -d www.pmsolution-facture.org --email ibhayk@gmail.com --agree-tos --no-eff-email
+
+# Reload Nginx to apply the new SSL certificates
+nginx -s reload
+
+# Keep the Nginx process running in the foreground
+wait $(cat /var/run/nginx.pid)
