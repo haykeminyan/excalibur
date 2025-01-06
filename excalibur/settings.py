b5a7f29deb918d549a91829d31d3c60c26ece7b0
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2025-01-06 16:56:14 +0000

    version 0.0.57 added sentry_sdk and some enhancements in settings.py

--- excalibur/settings.py
+++ excalibur/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY =os.getenv('SECRET_KEY', 'django-your-secret-key')
 
 # Initialize Sentry only once, adjust the settings based on the environment.
 sentry_sdk.init(
-    dsn=os.getenv('SENTRY_DSN'),
+    dsn=os.getenv('SENTRY_DSN', 'sentry-secret-key'),
     traces_sample_rate=1.0,  # Capture 100% of transactions
     send_default_pii=True,  # Include personally identifiable information (PII) in the event payload
     _experiments={
