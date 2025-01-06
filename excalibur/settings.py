ed55bd0d13988d713d53f79aa6d4a978bccf7697
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2025-01-06 17:44:22 +0000

    version 0.0.57 added django-axes

--- excalibur/settings.py
+++ excalibur/settings.py
@@ -85,6 +85,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'axes',
     'apps.main',
     'apps.deduction',
     'apps.facture',
@@ -113,7 +114,13 @@ LOGGING = {
         },
     },
 }
+AUTHENTICATION_BACKENDS = [
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    'axes.backends.AxesStandaloneBackend',
 
+    # Django ModelBackend is the default authentication backend.
+    'django.contrib.auth.backends.ModelBackend',
+]
 MIDDLEWARE = [
     'django.middleware.locale.LocaleMiddleware',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
@@ -126,6 +133,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'axes.middleware.AxesMiddleware',
 ]
 
 CACHE_MIDDLEWARE_ALIAS = 'default'
